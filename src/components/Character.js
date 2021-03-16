30fc6607c474553f429b313ec66d5bea4050b8db
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-16 07:42:52 +0200

    character.js use hooks

---
+++ src/components/Character.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+
+import Summary from "./Summary";
+
+const Character = (props) => {
+  const [loadedCharacter, setloadedCharacter] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = () => {
+    console.log(
+      "Sending Http request for new character with id " + props.selectedChar
+    );
+    setIsLoading(true);
+    fetch("https://swapi.dev/api/people/" + props.selectedChar)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Could not fetch person!");
+        }
+        return response.json();
+      })
+      .then((charData) => {
+        const loadedCharacter = {
+          id: props.selectedChar,
+          name: charData.name,
+          height: charData.height,
+          colors: {
+            hair: charData.hair_color,
+            skin: charData.skin_color,
+          },
+          gender: charData.gender,
+          movieCount: charData.films.length,
+        };
+        setloadedCharacter(loadedCharacter);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+    return () => {
+      console.log("clean...");
+    };
+  }, [props.selectedChar]);
+
+  let content = <p>Loading Character...</p>;
+
+  if (!isLoading && loadedCharacter.id) {
+    content = (
+      <Summary
+        name={loadedCharacter.name}
+        gender={loadedCharacter.gender}
+        height={loadedCharacter.height}
+        hairColor={loadedCharacter.colors.hair}
+        skinColor={loadedCharacter.colors.skin}
+        movieCount={loadedCharacter.movieCount}
+      />
+    );
+  } else if (!isLoading && !loadedCharacter.id) {
+    content = <p>Failed to fetch character.</p>;
+  }
+  return content;
+};
+
+export default React.memo(Character);
